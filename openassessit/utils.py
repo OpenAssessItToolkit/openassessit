407ea2cc2d235101c6f93975ae54237fed551ef6
Joel Crawford-Smith <jhc36@duke.edu>
2018-10-10 12:33:59 -0400

    Add generation of missing images and add more audits

--- openassessit/utils.py
+++ openassessit/utils.py
@@ -1,5 +1,5 @@
 import re
 
 def generate_img_filename(url, identifier):
-    '''generate useful filename with a max of 260 chars'''
+    """generate useful filename with a max of 260 chars"""
     return re.sub(r'\W', '-', '%s%s' % (url[0:36], identifier[-210:])) + '.png'
