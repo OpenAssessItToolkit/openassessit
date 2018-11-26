a5eca41807991bf4510407aac7659ed54ed5626e
Joel Crawford-Smith <jhc36@duke.edu>
2018-11-26 09:17:06 -0500

    Update optional run.sh to match the entrypoint behaviors

--- run.sh
+++ run.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo 'Requires, url, output filename, and path or alias to your Lighthouse install'
+if [ $# -ne 4 ]; then
+    echo 'Requires, url, output filename, path or alias to your Lighthouse, webdriver'
     exit 1
 fi
 $3 $1 \
 
-lighthouse $1 \
 --only-categories=accessibility \
---disable-device-emulation \
---output=json \
---output-path="$(pwd)/example/$2.json" \
---chrome-flags="--headless --window-size=1300,600"
---disable-device-emulation \
+--no-enable-error-reporting \
 --output="json" \
---output-path="$(pwd)/example/$2.json" \
---chrome-flags="--headless --window-size=1300,600";
+--output-path="$(pwd)/tmp/$2.json" \
+--chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu --disable-device-emulation";
 python3 $(pwd)/openassessit/markdown.py \
---input-file="$(pwd)/example/$2.json" \
---output-file="$(pwd)/example/$2.md" \
+--input-file="$(pwd)/tmp/$2.json" \
+--output-file="$(pwd)/tmp/$2.md" \
 --user-template-path="$(pwd)/../openassessit_templates/templates/";
+mkdir -p tmp/assets;
 python3 $(pwd)/openassessit/capture.py \
---input-file="$(pwd)/example/$2.json" \
---assets-dir="$(pwd)/example/assets/" \
+--input-file="$(pwd)/tmp/$2.json" \
+--assets-dir="$(pwd)/tmp/assets/" \
 --sleep=4 \
---driver=firefox
+--driver=$4
