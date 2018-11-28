483a6983b56cb21c7b5ff7a99c64c9429dac4558
Joel Crawford-Smith <jhc36@duke.edu>
2018-11-28 10:01:23 -0500

    Update run.sh to match entrypoint better

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 if [ $# -ne 4 ]; then
-    echo 'Requires, url, output filename, path or alias to your Lighthouse, webdriver'
+    echo 'Requires: path or alias to your Lighthouse, url, output filename, webdriver'
     exit 1
 fi
-$3 $1 \
-
+$1 $2 \
 --only-categories=accessibility \
 --no-enable-error-reporting \
+--disable-device-emulation \
+--chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu" \
 --output="json" \
---output-path="$(pwd)/tmp/$2.json" \
---chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu --disable-device-emulation";
+--output-path="$(pwd)/tmp/$3.json";
 python3 $(pwd)/openassessit/markdown.py \
---input-file="$(pwd)/tmp/$2.json" \
---output-file="$(pwd)/tmp/$2.md" \
+--input-file="$(pwd)/tmp/$3.json" \
+--output-file="$(pwd)/tmp/$3.md" \
 --user-template-path="$(pwd)/../openassessit_templates/templates/";
 mkdir -p tmp/assets;
 python3 $(pwd)/openassessit/capture.py \
---input-file="$(pwd)/tmp/$2.json" \
+--input-file="$(pwd)/tmp/$3.json" \
 --assets-dir="$(pwd)/tmp/assets/" \
 --sleep=4 \
 --driver=$4
