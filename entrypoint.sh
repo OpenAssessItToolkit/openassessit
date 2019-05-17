71ee8e69c59af676c2140aa6f2c29d3fd1fee1c3
Joel Crawford-Smith <jhc36@duke.edu>
2019-05-17 09:30:23 -0400

    Update Lighthouse cli flag to use the new --emulated-form-factor

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /tmp/$2/assets;
 cd openassessit;
-lighthouse $1 --only-categories=accessibility --no-enable-error-reporting --disable-device-emulation --chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu" --output="json" --output-path="/tmp/$2/$2.json";
+lighthouse $1 --only-categories=accessibility --no-enable-error-reporting --emulated-form-factor=none --chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu" --output="json" --output-path="/tmp/$2/$2.json";
 python3 -m openassessit.markdown --input-file="/tmp/$2/$2.json" --output-file="/tmp/$2/$2.md" --user-template-path="/app/openassessit_templates/templates/";
 python3 -m openassessit.to_html --input-file="/tmp/$2/$2.md" --output-file="/tmp/$2/$2.html" --user-template-path="/app/openassessit_templates/templates/";
 python3 -m openassessit.capture --input-file="/tmp/$2/$2.json" --assets-dir="/tmp/$2/assets/" --sleep=4 --driver=$3;
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ fi
 mkdir -p $(pwd)/tmp/$3;
 $1 $2 \
 --no-enable-error-reporting \
---disable-device-emulation \
+--emulated-form-factor=none \
 --only-categories=accessibility \
 --chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu" \
 --output="json" \
