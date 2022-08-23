0501cc1c23fa299d7caad52448b5a8c7de5734c2
Joel Crawford-Smith <jhc36@duke.edu>
2022-08-23 09:30:25 -0400

    change chrome path to be explicit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /tmp/$2/assets;
 cd openassessit;
-CHROME_PATH=/usr/bin/chromedriver lighthouse $1 --add-host="chrome:127.0.0.1" --only-categories=accessibility --no-enable-error-reporting --emulated-form-factor=none --chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu" --output="json" --output-path="/tmp/$2/$2.json";
+CHROME_PATH=/usr/bin/chromedriver lighthouse $1 --only-categories=accessibility --no-enable-error-reporting --emulated-form-factor=none --chrome-flags="--headless --window-size=1300,600 --no-sandbox --disable-gpu" --output="json" --output-path="/tmp/$2/$2.json";
 python3 -m openassessit.markdown --input-file="/tmp/$2/$2.json" --output-file="/tmp/$2/$2.md" --user-template-path="/app/openassessit_templates/templates/";
 python3 -m openassessit.to_html --input-file="/tmp/$2/$2.md" --output-file="/tmp/$2/$2.html" --user-template-path="/app/openassessit_templates/templates/";
 python3 -m openassessit.capture --input-file="/tmp/$2/$2.json" --assets-dir="/tmp/$2/assets/" --sleep=4 --driver=$3;
